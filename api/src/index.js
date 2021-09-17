7f73f1d546d8fc07e7337d1ad6db5d3c1add9414
Danielo64 <danyjogador64@gmail.com>
2021-09-17 15:12:53 -0300

    irra

--- api/src/index.js
+++ api/src/index.js
@@ -18,14 +18,10 @@ app.get('/matricula', async (req, resp) => {
 app.post('/matricula', async (req, resp) => {
     try {
         let { nome, chamada, curso, turma } = req.body;
-
+        
         if (chamada <= 0)
-            return resp.send({ erro: 'Chamada deve ser maior que zero.' });
+            return resp.send({ erro: 'Chamada deve ser maior que zero!' });
         
-        if (!nome || nome.replace(/\n/g, '') == '')
-            return resp.send({ erro: 'Nome é obrigatório!' });
-        
-
         let r = await db.tb_matricula.create(
         {
             nm_aluno: nome,
